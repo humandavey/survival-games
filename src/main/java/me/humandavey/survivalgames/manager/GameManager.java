3c52fc97fc81be1fe5b313daea2b3390c0b83070
humandavey <davey.adams.three@gmail.com>
2023-08-07 15:25:28 -0500

    Update countdown logic

--- src/main/java/me/humandavey/survivalgames/manager/GameManager.java
+++ src/main/java/me/humandavey/survivalgames/manager/GameManager.java
@@ -2,6 +2,7 @@ package me.humandavey.survivalgames.manager;
 
 import me.humandavey.survivalgames.SurvivalGames;
 import me.humandavey.survivalgames.instance.Countdown;
+import me.humandavey.survivalgames.util.MessageUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
@@ -34,12 +35,15 @@ public class GameManager {
 		players.remove(player.getUniqueId());
 
 		if (SurvivalGames.getConfiguration().getMinPlayers() > players.size() && countdown != null) {
+			if (!countdown.isRunning()) return;
 			cancelCountdown();
 		}
 	}
 
 	public void startCountdown() {
-		cancelCountdown();
+		if (countdown != null) {
+			cancelCountdown();
+		}
 
 		countdown = new Countdown();
 		countdown.start();
@@ -49,6 +53,13 @@ public class GameManager {
 		countdown.setCancel(true);
 		countdown.cancel();
 		countdown = null;
+
+		for (Player player : SurvivalGames.getGameManager().getPlayers()) {
+			player.setLevel(0);
+			player.setExp(0);
+		}
+
+		SurvivalGames.getGameManager().broadcast(MessageUtil.COUNTDOWN_CANCELLED.getMessage());
 	}
 
 	public ArrayList<Player> getPlayers() {
