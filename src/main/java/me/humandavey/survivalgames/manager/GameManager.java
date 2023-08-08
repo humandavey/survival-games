b848339ad35a74b3e980eb658c1cdb6548b99f29
humandavey <davey.adams.three@gmail.com>
2023-08-07 20:48:11 -0500

    Add chest logic

--- src/main/java/me/humandavey/survivalgames/manager/GameManager.java
+++ src/main/java/me/humandavey/survivalgames/manager/GameManager.java
@@ -4,9 +4,11 @@ import me.humandavey.survivalgames.SurvivalGames;
 import me.humandavey.survivalgames.instance.Countdown;
 import me.humandavey.survivalgames.util.MessageUtil;
 import org.bukkit.Bukkit;
+import org.bukkit.block.Chest;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.UUID;
 
 public class GameManager {
@@ -14,6 +16,29 @@ public class GameManager {
 	private final ArrayList<UUID> players = new ArrayList<>();
 	private GameState state = GameState.WAITING;
 	private Countdown countdown;
+
+	public void onStart() {
+		fillChests();
+	}
+
+	private void fillChest(Chest chest, int tier) {
+		if (SurvivalGames.getConfiguration().getItems(tier).size() < 1) return;
+		int numToPick = new Random().nextInt(5, 9);
+		for (int i = 0; i < numToPick; i++) {
+			int index = new Random().nextInt(0, SurvivalGames.getConfiguration().getItems(tier).size());
+			int slot = new Random().nextInt(0, 27);
+			chest.getBlockInventory().setItem(slot, SurvivalGames.getConfiguration().getItems(tier).get(index));
+		}
+	}
+
+	public void fillChests() {
+		for (Chest chest : SurvivalGames.getConfiguration().getChests(1)) {
+			fillChest(chest, 1);
+		}
+		for (Chest chest : SurvivalGames.getConfiguration().getChests(2)) {
+			fillChest(chest, 2);
+		}
+	}
 
 	public void broadcast(String message) {
 		for (UUID uuid : players) {
